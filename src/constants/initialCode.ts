fbc07f25be897bced33e7b0027d88f3215a80a2e
hota1024 <hotalog@hota1024.com>
2021-09-22 23:18:07 +0900

    :poop: fix initial code

--- src/constants/initialCode.ts
+++ src/constants/initialCode.ts
@@ -2,13 +2,13 @@ export const initialCode = `
 // app class.
 class App extends QratchApp {
   frame() {
-    const { renderer, drawer } = this
+    const { cursor, drawer, renderer  } = this
 
     // clear screen with black.
     renderer.fill('black')
 
     // draw red circle at cursor position.
-    drawer.fillArc(this.cursor, 32, 0, Math.PI * 2, 'red')
+    drawer.fillArc(cursor, 32, 0, Math.PI * 2, 'red')
   }
 }
 
