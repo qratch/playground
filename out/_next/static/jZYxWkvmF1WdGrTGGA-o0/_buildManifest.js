92f5992c7b345107e0a18932bf0b980e1e2e874d
hota1024 <hotalog@hota1024.com>
2021-10-16 10:49:35 +0900

    🚀 deploy latest qratch

---
+++ out/_next/static/jZYxWkvmF1WdGrTGGA-o0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-804b3e5cfb79ff7a3636.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b243c7ba2f2b53dc8e53.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
